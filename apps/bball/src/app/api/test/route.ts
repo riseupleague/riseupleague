e1f25a52940d933ab3808195d75a1ff8b9445e74
n9d0g <nathanjlardizabal@gmail.com>
2023-09-11 02:31:52 -0400

    test(bball): `no-store` cache test in api call

--- apps/bball/src/app/api/test/route.ts
+++ apps/bball/src/app/api/test/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request): Promise<NextResponse> {
-	const res = await fetch("https://dog.ceo/api/breeds/image/random");
+	const res = await fetch("https://dog.ceo/api/breeds/image/random", {
+		cache: "no-store",
+	});
 	const data = await res.json();
 
 	return NextResponse.json({ data });
