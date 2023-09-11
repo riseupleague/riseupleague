5b3e5bf1bb8f295afc62e6e26df4a97a55bd1c06
n9d0g <nathanjlardizabal@gmail.com>
2023-09-11 01:20:40 -0400

    revert(bball): take back api testing code

--- apps/bball/src/app/standings/page.tsx
+++ apps/bball/src/app/standings/page.tsx
@@ -1,16 +1,16 @@
-async function getData() {
-	const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+// async function getData() {
+// 	const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-	const res = await fetch(`${baseURL}/api/test`);
-	if (!res.ok) throw new Error("Cannot fetch standings data");
+// 	const res = await fetch(`${baseURL}/api/test`);
+// 	if (!res.ok) throw new Error("Cannot fetch standings data");
 
-	return res.json();
-}
+// 	return res.json();
+// }
 
-export default async function Standings(): Promise<JSX.Element> {
-	// export default function Standings(): JSX.Element {
-	const { data } = await getData();
-	console.log(data);
+// export default async function Standings(): Promise<JSX.Element> {
+export default function Standings(): JSX.Element {
+	// const { data } = await getData();
+	// console.log(data);
 
 	return (
 		<section className="container mx-auto flex min-h-screen items-center justify-center">
