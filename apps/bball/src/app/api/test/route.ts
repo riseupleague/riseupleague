9a16cff981ee00109a6c25c9ef39612e3650d7b4
n9d0g <nathanjlardizabal@gmail.com>
2023-09-11 01:06:51 -0400

    fix(bball): fix api test

--- apps/bball/src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-	const res = await fetch("https://dog.ceo/api/breeds/image/random");
-	const data = await res.json();
-
-	return NextResponse.json({ data });
-}
