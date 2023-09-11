5470e65a24be18519d3dd0c6fc29e5364e8b1be2
n9d0g <nathanjlardizabal@gmail.com>
2023-09-11 02:23:42 -0400

    test(bball): add `request` to `GET` handler test

--- apps/bball/src/app/api/test/route.ts
+++ apps/bball/src/app/api/test/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request): Promise<NextResponse> {
 	const res = await fetch("https://dog.ceo/api/breeds/image/random");
 	const data = await res.json();
 
