ee7a34571353d6af5550e47ec8038eecf0163be1
n9d0g <nathanjlardizabal@gmail.com>
2023-09-11 02:44:30 -0400

    test(bball): put API call in main RSC js logic space

--- apps/bball/src/app/standings/page.tsx
+++ apps/bball/src/app/standings/page.tsx
@@ -1,18 +1,13 @@
-// async function getData() {
-// 	const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export default async function Standings(): Promise<JSX.Element> {
+	// export default function Standings(): JSX.Element {
+	const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-// 	const res = await fetch(`${baseURL}/api/test`, { cache: "no-store" });
-// 	if (!res.ok) throw new Error("Cannot fetch standings data");
+	const res = await fetch(`${baseURL}/api/test`, { cache: "no-store" });
+	if (!res.ok) throw new Error("Cannot fetch standings data");
 
-// 	return res.json();
-// }
+	const { data } = await res.json();
 
-// export default async function Standings(): Promise<JSX.Element> {
-export default function Standings(): JSX.Element {
-	// const { data } = await getData();
-
-	// const dogMessage = data?.message;
-	// if (data) console.log(dogMessage);
+	console.log(data);
 
 	return (
 		<section className="container mx-auto flex min-h-screen items-center justify-center">
