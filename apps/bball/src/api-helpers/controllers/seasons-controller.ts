3b6aafdf08efd993cb0f93dc75223c512f9b33fc
n9d0g <nathanjlardizabal@gmail.com>
2023-09-11 18:36:22 -0400

    feat(bball): api helpers -> seasons controller

--- apps/bball/src/api-helpers/controllers/seasons-controller.ts
+++ apps/bball/src/api-helpers/controllers/seasons-controller.ts
@@ -3,11 +3,25 @@ import { NextResponse } from "next/server";
 
 export const getAllSeasons = async () => {
 	try {
-		let seasons = await Season.find();
+		const seasons = await Season.find();
+
 		return NextResponse.json({ seasons });
 	} catch (e) {
 		return NextResponse.json(
 			{ message: "error retrieving all seasons" },
+			{ status: 500 }
+		);
+	}
+};
+
+export const getActiveSeason = async () => {
+	try {
+		const season = await Season.find({ active: true });
+
+		return NextResponse.json({ season });
+	} catch (e) {
+		return NextResponse.json(
+			{ message: "error retrieving active season" },
 			{ status: 500 }
 		);
 	}
--- apps/bball/src/app/api/seasons/route.ts
+++ apps/bball/src/app/api/seasons/route.ts
@@ -1,15 +1,32 @@
 import { connectToDatabase } from "@/src/api-helpers/utils";
-import { getAllSeasons } from "@/src/api-helpers/controllers/seasons-controller";
+import {
+	getActiveSeason,
+	getAllSeasons,
+} from "@/src/api-helpers/controllers/seasons-controller";
 import { NextResponse } from "next/server";
 
 export const GET = async (req: Request) => {
 	try {
 		await connectToDatabase();
+
+		const { searchParams } = new URL(req.url);
+		const active = searchParams.get("active");
+
+		// if active param in request
+		if (active) {
+			const res = await getActiveSeason();
+			const { season } = await res.json();
+
+			return NextResponse.json(season, { status: 200 });
+		}
+
+		// if not, get all seasons
 		const res = await getAllSeasons();
 		const { seasons } = await res.json();
 
 		return NextResponse.json(seasons, { status: 200 });
 	} catch (e) {
+		console.log(e);
 		return NextResponse.json(e, { status: 500 });
 	}
 };
