f41acd58251ef3b7ed60f61f2b2abdcffc97fc9b
n9d0g <nathanjlardizabal@gmail.com>
2023-09-14 14:05:21 -0400

    feat(bball): `/players` page search functionality; route groups

--- apps/bball/src/components/general/PlayerCard.tsx
+++ apps/bball/src/components/general/PlayerCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import thirtyPtBadge from "@/public/images/badges/thirtyPtBadge.svg";
---
+++ apps/bball/src/components/players/PlayerGrid.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import PlayerCard from "@/src/components/general/PlayerCard";
+import { useState } from "react";
+
+export default function PlayerGrid({ allPlayers }) {
+	const [players, setPlayers] = useState(allPlayers);
+
+	const handleSearch = (e) => {
+		const searchValue = e.target.value;
+
+		// empty search
+		if (searchValue === "") return setPlayers(allPlayers);
+
+		setPlayers(
+			players.filter((player) =>
+				player.playerName.toLowerCase().includes(searchValue.toLowerCase())
+			)
+		);
+	};
+
+	return (
+		<>
+			<input
+				type="search"
+				className="my-4 p-2 text-black"
+				onChange={handleSearch}
+			/>
+			{players.length > 0 ? (
+				<div className="grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 sm:px-0 lg:grid-cols-3 2xl:grid-cols-4">
+					{players.map((player, index) => (
+						<PlayerCard player={player} key={index} />
+					))}
+				</div>
+			) : (
+				<div className="flex h-[50dvh] items-center justify-center">
+					<p className="font-barlow text-4xl uppercase">
+						no search results found!
+					</p>
+				</div>
+			)}
+		</>
+	);
+}
