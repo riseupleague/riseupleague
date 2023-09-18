a5205cffa9ae6090f9d5955d5eb371977278ec40
n9d0g <nathanjlardizabal@gmail.com>
2023-09-18 18:08:32 -0400

    feat(bball): use shadcn `Input` component

--- apps/bball/src/components/players/PlayerGrid.tsx
+++ apps/bball/src/components/players/PlayerGrid.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import PlayerCard from "@/src/components/general/PlayerCard";
 import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import PlayerCard from "@/src/components/general/PlayerCard";
 
 export default function PlayerGrid({ allPlayers }) {
 	const [players, setPlayers] = useState(allPlayers);
@@ -21,11 +22,14 @@ export default function PlayerGrid({ allPlayers }) {
 
 	return (
 		<>
-			<input
-				type="search"
-				className="my-4 p-2 text-neutral-900"
-				onChange={handleSearch}
-			/>
+			<div className="relative w-fit">
+				<Input
+					type="text"
+					placeholder="Search Player"
+					onChange={handleSearch}
+					className="font-barlow my-4 border-neutral-600 bg-neutral-800 px-4 py-2 text-lg"
+				/>
+			</div>
 			{players.length > 0 ? (
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:px-0 lg:grid-cols-3 2xl:grid-cols-4">
 					{players.map((player, index) => (
