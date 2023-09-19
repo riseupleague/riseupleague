3568ad800d753327ed521f99f3a00d43d2f64c05
n9d0g <nathanjlardizabal@gmail.com>
2023-09-18 22:31:11 -0400

    feat(bball): future games scaffold

--- apps/bball/src/components/structure/header/header.tsx
+++ apps/bball/src/components/structure/header/header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import PrimaryHeader from "./primary-header";
 import SecondaryHeader from "./secondary-header";
+import { connectToDatabase } from "@/src/api-helpers/utils";
+import { getAllPlayers } from "@/src/api-helpers/controllers/players-controller";
 
-export default function Header(): React.JSX.Element {
-	// fetch secondary header data (ie. past/future game API)
+export default async function Header(): Promise<React.JSX.Element> {
+	await connectToDatabase();
+	const res = await getAllPlayers();
+	const { allPlayers } = await res.json();
 
 	return (
 		<header>
