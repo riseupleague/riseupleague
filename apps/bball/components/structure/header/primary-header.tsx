16adf67d4db117a6f5c518095c7cc9a9558a9d24
johnBaloran <jdbalora@gmail.com>
2023-10-19 10:02:27 -0400

    navigation menu for mobile

--- apps/bball/components/structure/header/primary-header.tsx
+++ apps/bball/components/structure/header/primary-header.tsx
@@ -8,13 +8,6 @@ import { Button } from "@ui/components/button";
 import { Separator } from "@ui/components/separator";
 
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from "@ui/components/accordion";
-
-import {
 	Sheet,
 	SheetClose,
 	SheetContent,
@@ -279,98 +272,44 @@ export default function PrimaryHeader(): React.JSX.Element {
 										className={`px-6 py-2 ${isActive && "bg-neutral-600"}`}
 										key={index}
 									>
-										<Link
-											href={option.href}
-											className="flex items-center gap-3"
-										>
-											{option.icon}
-											<span className="font-barlow text-[24px] uppercase tracking-tighter">
-												{option.label}
-											</span>
-										</Link>
+										<SheetClose asChild>
+											<Button variant="navlink" size="navlink">
+												<a
+													href={option.href}
+													className="flex items-center gap-3"
+												>
+													{option.icon}
+													<span className="font-barlow text-[24px] uppercase tracking-tighter">
+														{option.label}
+													</span>
+												</a>
+											</Button>
+										</SheetClose>
 									</li>
 								);
 							})}
 
 							<Separator className="mb-4 bg-neutral-600" />
-
-							<Accordion type="single" collapsible className="w-full">
-								<AccordionItem value="item-1" className="border-0  px-6">
-									<AccordionTrigger>
-										<li className="flex items-center gap-3">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												width="19"
-												height="19"
-												viewBox="0 0 19 19"
-												fill="none"
-											>
-												<path
-													d="M7.82087 5.95254C8.74792 5.14108 10.2521 5.14108 11.1791 5.95254C12.107 6.764 12.107 8.07975 11.1791 8.89121C11.0184 9.03292 10.8387 9.14929 10.6487 9.24113C10.0589 9.52692 9.50079 10.032 9.50079 10.6875V11.2813M16.625 9.5C16.625 10.4357 16.4407 11.3622 16.0826 12.2266C15.7246 13.0911 15.1998 13.8765 14.5381 14.5381C13.8765 15.1998 13.0911 15.7246 12.2266 16.0826C11.3622 16.4407 10.4357 16.625 9.5 16.625C8.56433 16.625 7.63783 16.4407 6.77338 16.0826C5.90894 15.7246 5.12348 15.1998 4.46186 14.5381C3.80025 13.8765 3.27542 13.0911 2.91736 12.2266C2.55929 11.3622 2.375 10.4357 2.375 9.5C2.375 7.61033 3.12567 5.79806 4.46186 4.46186C5.79806 3.12567 7.61033 2.375 9.5 2.375C11.3897 2.375 13.2019 3.12567 14.5381 4.46186C15.8743 5.79806 16.625 7.61033 16.625 9.5ZM9.5 13.6563H9.50633V13.6626H9.5V13.6563Z"
-													stroke="#ABAFB3"
-													stroke-width="1.5"
-													stroke-linecap="round"
-													stroke-linejoin="round"
-												/>
-											</svg>
-											<span className="font-barlow text-[24px] uppercase tracking-tighter">
-												League
-											</span>
-										</li>
-									</AccordionTrigger>
-									<AccordionContent>
-										<ul className="mt-5 flex flex-col gap-6">
-											<li>
-												<Link
-													href="/about-us"
-													className="flex items-center gap-3"
-												>
-													<span className="font-barlow text-[20px] uppercase tracking-tighter">
-														About Us
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													href="/league-rules"
-													className="flex items-center gap-3"
-												>
-													<span className="font-barlow text-[20px] uppercase tracking-tighter">
-														League Rules
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													href="/about-us"
-													className="flex items-center gap-3"
-												>
-													<span className="font-barlow text-[20px] uppercase tracking-tighter">
-														Terms And Conditions
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link href="/faq" className="flex items-center gap-3">
-													<span className="font-barlow text-[20px] uppercase tracking-tighter">
-														FAQ
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													href="/contact-us"
-													className="flex items-center gap-3"
-												>
-													<span className="font-barlow text-[20px] uppercase tracking-tighter">
-														Contact Us
-													</span>
-												</Link>
-											</li>
-										</ul>
-									</AccordionContent>
-								</AccordionItem>
-							</Accordion>
+							<li className="flex items-center gap-3 px-6 py-2">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="19"
+									height="19"
+									viewBox="0 0 19 19"
+									fill="none"
+								>
+									<path
+										d="M7.82087 5.95254C8.74792 5.14108 10.2521 5.14108 11.1791 5.95254C12.107 6.764 12.107 8.07975 11.1791 8.89121C11.0184 9.03292 10.8387 9.14929 10.6487 9.24113C10.0589 9.52692 9.50079 10.032 9.50079 10.6875V11.2813M16.625 9.5C16.625 10.4357 16.4407 11.3622 16.0826 12.2266C15.7246 13.0911 15.1998 13.8765 14.5381 14.5381C13.8765 15.1998 13.0911 15.7246 12.2266 16.0826C11.3622 16.4407 10.4357 16.625 9.5 16.625C8.56433 16.625 7.63783 16.4407 6.77338 16.0826C5.90894 15.7246 5.12348 15.1998 4.46186 14.5381C3.80025 13.8765 3.27542 13.0911 2.91736 12.2266C2.55929 11.3622 2.375 10.4357 2.375 9.5C2.375 7.61033 3.12567 5.79806 4.46186 4.46186C5.79806 3.12567 7.61033 2.375 9.5 2.375C11.3897 2.375 13.2019 3.12567 14.5381 4.46186C15.8743 5.79806 16.625 7.61033 16.625 9.5ZM9.5 13.6563H9.50633V13.6626H9.5V13.6563Z"
+										stroke="#ABAFB3"
+										stroke-width="1.5"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									/>
+								</svg>
+								<span className="font-barlow text-[24px] uppercase tracking-tighter">
+									League
+								</span>
+							</li>
 						</ul>
 					</SheetContent>
 				</Sheet>
