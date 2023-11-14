7413f04e5c4243e78803b0ea86cae8ad71b34620
n9d0g <nathanjlardizabal@gmail.com>
2023-11-14 03:31:54 -0500

    fix(bball): `FeaturedPlayerCard` UI fixes

--- apps/bball/components/general/FeaturedPlayerCard.tsx
+++ apps/bball/components/general/FeaturedPlayerCard.tsx
@@ -61,110 +61,112 @@ export default function FeaturedPlayerCard({ player, division }) {
 	}
 
 	return (
-		<div className="h-fit rounded border border-neutral-600 bg-neutral-700">
-			{/* pic / name / team / division */}
-			<div className="flex justify-between gap-3 border-b border-neutral-600 px-6 py-3.5">
-				<figure className="h-[52px] w-[52px] rounded-full bg-[#d9d9d9]"></figure>
-				<div className="flex w-full flex-col">
-					<div className="flex w-full justify-between">
-						<h6 className="font-barlow font-medium uppercase text-neutral-500">
-							team {player.team?.substring(0, 5)}... | #{player.jerseyNumber}
-						</h6>
-						<div className="flex justify-center rounded-md bg-neutral-600 px-4 py-1">
-							<p className="font-barlow text-xs font-medium uppercase">
-								{division}
-							</p>
+		<div className="flex h-full flex-col rounded border border-neutral-600 bg-neutral-700">
+			<div className="flex-1">
+				{/* pic / name / team / division */}
+				<div className="flex justify-between gap-3 border-b border-neutral-600 px-6 py-3.5">
+					<figure className="h-[52px] w-[52px] rounded-full bg-[#d9d9d9]"></figure>
+					<div className="flex w-full flex-col">
+						<div className="flex w-full justify-between">
+							<h6 className="font-barlow font-medium uppercase text-neutral-500">
+								team {player.team?.substring(0, 5)}... | #{player.jerseyNumber}
+							</h6>
+							<div className="flex justify-center rounded-md bg-neutral-600 px-4 py-1">
+								<p className="font-barlow text-xs font-medium uppercase">
+									{division}
+								</p>
+							</div>
 						</div>
-					</div>
-					<Link
-						href={`/players/${player._id}`}
-						className="font-barlow w-fit truncate text-2xl uppercase text-neutral-100 transition hover:opacity-80"
-					>
-						{player.playerName}
-					</Link>
-				</div>
-			</div>
-
-			{/* stats table */}
-			<div className="h-fit border-b border-neutral-600"></div>
-
-			{/* badges */}
-			<div className="flex justify-center gap-x-2 border-b border-neutral-600 px-5 py-3">
-				{badges.map((badge, index) => {
-					const hasBadge = badge.length > 0;
-
-					return (
-						<div
-							className={`flex h-[60px] w-[60px] justify-center rounded-xl bg-neutral-600 ${
-								hasBadge &&
-								"cursor-pointer transition hover:scale-150 hover:border hover:border-neutral-300"
-							}`}
-							key={index}
+						<Link
+							href={`/players/${player._id}`}
+							className="font-barlow w-fit truncate text-2xl uppercase text-neutral-100 transition hover:opacity-80"
 						>
-							{hasBadge ? (
-								<Image
-									src={badge}
-									alt={""}
-									width={200}
-									height={200}
-									className="h-full w-auto"
-								/>
-							) : (
-								""
-							)}
-						</div>
-					);
-				})}
-			</div>
+							{player.playerName}
+						</Link>
+					</div>
+				</div>
 
-			{/* previous games table */}
-			<Table>
-				<TableHeader>
-					<TableRow className="font-oswald border-b-neutral-600 uppercase">
-						<TableHead className="px-2 text-sm font-normal">game</TableHead>
-						{/* <TableHead className="px-2 text-sm font-normal">matchup</TableHead> */}
-						<TableHead className="px-2 text-sm font-normal">pts</TableHead>
-						<TableHead className="px-2 text-sm font-normal">reb</TableHead>
-						<TableHead className="px-2 text-sm font-normal">ast</TableHead>
-						<TableHead className="px-2 text-sm font-normal">stl</TableHead>
-						<TableHead className="px-2 text-sm font-normal">blk</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{allStats
-						?.map((game, index) => (
-							<TableRow
-								className="font-oswald border-b-0 text-sm uppercase"
+				{/* stats table */}
+				<div className="h-fit border-b border-neutral-600"></div>
+
+				{/* badges */}
+				<div className="flex justify-center gap-x-2 border-b border-neutral-600 px-5 py-3">
+					{badges.map((badge, index) => {
+						const hasBadge = badge.length > 0;
+
+						return (
+							<div
+								className={`flex h-[60px] w-[60px] justify-center rounded-xl bg-neutral-600 ${
+									hasBadge &&
+									"cursor-pointer transition hover:scale-150 hover:border hover:border-neutral-300"
+								}`}
 								key={index}
 							>
-								<TableCell className="py-2">
-									{allStats.length - index}
-								</TableCell>
-								{/* <TableCell className="py-2">???</TableCell> */}
-								<TableCell className="py-2">{game.points}</TableCell>
-								<TableCell className="py-2">{game.rebounds}</TableCell>
-								<TableCell className="py-2">{game.assists}</TableCell>
-								<TableCell className="py-2">{game.steals}</TableCell>
-								<TableCell className="py-2">{game.blocks}</TableCell>
-							</TableRow>
-						))
-						.slice(0, 4)}
-				</TableBody>
-			</Table>
+								{hasBadge ? (
+									<Image
+										src={badge}
+										alt={""}
+										width={200}
+										height={200}
+										className="h-full w-auto"
+									/>
+								) : (
+									""
+								)}
+							</div>
+						);
+					})}
+				</div>
 
-			{/* avg stats */}
-			<div className="flex justify-center gap-4 border-b border-neutral-600 px-4 py-3">
-				{avgStats.slice(0, 3).map((stat, index) => (
-					<div
-						key={index}
-						className="font-barlow flex w-full flex-col justify-center rounded-lg bg-neutral-600 py-4"
-					>
-						<h6 className="text-center">{stat.label}</h6>
-						<h3 className="text-center text-[31px] font-medium">
-							{stat.average.toFixed(1)}
-						</h3>
-					</div>
-				))}
+				{/* previous games table */}
+				<Table>
+					<TableHeader>
+						<TableRow className="font-oswald border-b-neutral-600 uppercase">
+							<TableHead className="px-2 text-sm font-normal">game</TableHead>
+							{/* <TableHead className="px-2 text-sm font-normal">matchup</TableHead> */}
+							<TableHead className="px-2 text-sm font-normal">pts</TableHead>
+							<TableHead className="px-2 text-sm font-normal">reb</TableHead>
+							<TableHead className="px-2 text-sm font-normal">ast</TableHead>
+							<TableHead className="px-2 text-sm font-normal">stl</TableHead>
+							<TableHead className="px-2 text-sm font-normal">blk</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{allStats
+							?.map((game, index) => (
+								<TableRow
+									className="font-oswald border-b-0 text-sm uppercase"
+									key={index}
+								>
+									<TableCell className="py-2">
+										{allStats.length - index}
+									</TableCell>
+									{/* <TableCell className="py-2">???</TableCell> */}
+									<TableCell className="py-2">{game.points}</TableCell>
+									<TableCell className="py-2">{game.rebounds}</TableCell>
+									<TableCell className="py-2">{game.assists}</TableCell>
+									<TableCell className="py-2">{game.steals}</TableCell>
+									<TableCell className="py-2">{game.blocks}</TableCell>
+								</TableRow>
+							))
+							.slice(0, 4)}
+					</TableBody>
+				</Table>
+
+				{/* avg stats */}
+				<div className="flex justify-center gap-4 border-b border-neutral-600 px-4 py-3">
+					{avgStats.slice(0, 3).map((stat, index) => (
+						<div
+							key={index}
+							className="font-barlow flex w-full flex-col justify-center rounded-lg bg-neutral-600 py-4"
+						>
+							<h6 className="text-center">{stat.label}</h6>
+							<h3 className="text-center text-[31px] font-medium">
+								{stat.average.toFixed(1)}
+							</h3>
+						</div>
+					))}
+				</div>
 			</div>
 
 			{/* view player profile */}
