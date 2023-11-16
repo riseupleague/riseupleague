b5d94d6a0fead50985938e04648cea7cbae3b245
n9d0g <nathanjlardizabal@gmail.com>
2023-11-16 17:00:27 -0500

    fix(bball): ToS + refund policy

---
+++ apps/bball/app/(footer)/terms-and-conditions/page.tsx
@@ -0,0 +1,118 @@
+export default function TermsAndConditions(): JSX.Element {
+	return (
+		<section className="container mx-auto px-4 md:px-0">
+			<h1 className="font-barlow mb-16 mt-8 text-center text-5xl font-medium uppercase">
+				Rise Up Basketball Terms and Conditions
+			</h1>
+
+			<div className="font-barlow text-lg">
+				<p className="my-4">
+					Welcome to Rise Up Basketball League! By participating in our
+					recreational basketball league, you agree to comply with the following
+					terms and conditions. Please read these terms carefully before
+					proceeding with your registration.
+				</p>
+
+				<h2 className="text-3xl">1. Code of Conduct:</h2>
+
+				<p className="my-4">
+					1.1. All participants, including players, coaches, officials, and
+					spectators, are expected to conduct themselves in a sportsmanlike
+					manner at all times. Unsportsmanlike behavior, including but not
+					limited to foul language, aggressive conduct, and disrespectful
+					actions, will not be tolerated.
+				</p>
+
+				<p className="my-4">
+					1.2. Any form of discrimination, harassment, or bullying based on
+					race, gender, religion, or any other characteristic is strictly
+					prohibited within our league.
+				</p>
+
+				<hr className="my-4 border-neutral-500" />
+
+				<h2 className="text-3xl">2. Participation Requirements:</h2>
+
+				<p className="my-4">
+					2.1. Participants must adhere to the league&apos;s schedule and rules
+					as outlined by the league organizers. Failure to comply may result in
+					penalties or disqualification.
+				</p>
+
+				<p className="my-4">
+					2.2. All players must meet the league&apos;s eligibility requirements,
+					including age restrictions, if applicable. Proof of age or
+					identification may be required.
+				</p>
+
+				<hr className="my-4 border-neutral-500" />
+
+				<h2 className="text-3xl">3. Liability and Waiver:</h2>
+
+				<p className="my-4">
+					3.1. Participants understand and acknowledge that basketball is a
+					physical sport that carries inherent risks. Rise Up Basketball League
+					and its organizers will not be held responsible for any injuries,
+					accidents, or damages that may occur during league activities.
+				</p>
+
+				<p className="my-4">
+					3.2. By participating in the league, all participants agree to waive
+					any claims or demands for liability against Rise Up Basketball League,
+					its organizers, sponsors, and affiliated parties.
+				</p>
+
+				<hr className="my-4 border-neutral-500" />
+
+				<h2 className="text-3xl">4. Photography and Media:</h2>
+
+				<p className="my-4">
+					4.1. Rise Up Basketball League may capture photos or videos of league
+					activities for promotional and documentation purposes. By
+					participating, you grant permission for your likeness to be used in
+					league-related materials.
+				</p>
+
+				<hr className="my-4 border-neutral-500" />
+
+				<h2 className="text-3xl">5. Changes and Updates:</h2>
+
+				<p className="my-4">
+					5.1. Rise Up Basketball League reserves the right to make changes to
+					the schedule, rules, policies, and other aspects of the league.
+					Participants will be notified of any significant changes in a timely
+					manner.
+				</p>
+
+				<hr className="my-4 border-neutral-500" />
+
+				<h2 className="text-3xl">6. Contact Information:</h2>
+
+				<p className="my-4">
+					For inquiries or assistance related to our terms and conditions,
+					please contact our league&apos;s customer service at{" "}
+					<a
+						className="text-primary transition-all hover:underline"
+						href="mailto:riseupbballleague@gmail.com
+"
+					>
+						riseupbballleague@gmail.com
+					</a>{" "}
+				</p>
+
+				<p className="my-4">
+					Please review and acknowledge these terms before proceeding with your
+					registration.
+				</p>
+
+				<p className="my-4">Last Updated: August 8, 2023</p>
+
+				<p className="my-4">
+					By proceeding with your registration, you confirm that you have read,
+					understood, and agreed to abide by the terms and conditions of Rise Up
+					Basketball League.
+				</p>
+			</div>
+		</section>
+	);
+}
