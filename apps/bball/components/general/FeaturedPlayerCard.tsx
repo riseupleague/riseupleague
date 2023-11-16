16782c485130ada85e3c91847834f4313925b0d7
n9d0g <nathanjlardizabal@gmail.com>
2023-11-16 00:09:21 -0500

    fix(bball): featured players responsive fixes

--- apps/bball/components/general/FeaturedPlayerCard.tsx
+++ apps/bball/components/general/FeaturedPlayerCard.tsx
@@ -54,48 +54,49 @@ export default function FeaturedPlayerCard({ player }) {
 	}
 
 	return (
-		<div className="h-fit rounded border border-neutral-600 bg-neutral-700">
-			{/* pic / name / team / division */}
-			<div className="flex justify-between gap-3 border-b border-neutral-600 px-6 py-3.5">
-				<TeamLogo
-					primary={player.team.primaryColor}
-					secondary={player.team.secondaryColor}
-					tertiary={player.team.tertiaryColor}
-					width={35}
-					height={34}
-					circleHeight={3}
-					circleWidth={4}
-				/>
-				{/* <figure className="h-[52px] w-[52px] rounded-full bg-[#d9d9d9]"></figure> */}
-				<div className="flex w-full flex-col">
-					<div className="flex w-full justify-between">
-						{/* <h6 className="font-barlow font-medium uppercase text-neutral-500">
+		<div className="h-full rounded border border-neutral-600 bg-neutral-700">
+			<div className="flex flex-col">
+				{/* pic / name / team / division */}
+				<div className="flex items-center justify-between gap-3 border-b border-neutral-600 px-6 py-3.5 md:min-h-[120px] 2xl:min-h-0">
+					<TeamLogo
+						primary={player.team.primaryColor}
+						secondary={player.team.secondaryColor}
+						tertiary={player.team.tertiaryColor}
+						width={35}
+						height={34}
+						circleHeight={3}
+						circleWidth={4}
+					/>
+					{/* <figure className="h-[52px] w-[52px] rounded-full bg-[#d9d9d9]"></figure> */}
+					<div className="flex w-full flex-col">
+						<div className="flex w-full justify-between">
+							{/* <h6 className="font-barlow font-medium uppercase text-neutral-500">
 							team {player.team?.teamName.substring(0, 5)}... | #
 							{player.jerseyNumber}
 						</h6> */}
-						<h6 className="font-barlow text-sm font-medium uppercase text-neutral-500">
-							{player.team?.teamName} | #{player.jerseyNumber}
-						</h6>
-						<div className="flex justify-center rounded-md bg-neutral-600 px-4 py-1">
-							<p className="font-barlow text-xs font-medium uppercase">
-								{player.division.divisionName}
-							</p>
+							<h6 className="font-barlow text-sm font-medium uppercase text-neutral-500">
+								{player.team?.teamName} | #{player.jerseyNumber}
+							</h6>
+							<div className="flex justify-center rounded-md bg-neutral-600 px-4 py-1">
+								<p className="font-barlow text-xs font-medium uppercase">
+									{player.division.divisionName}
+								</p>
+							</div>
 						</div>
+						<Link
+							href={`/players/${player._id}`}
+							className="font-barlow w-fit truncate text-lg uppercase text-neutral-100 transition hover:opacity-80"
+						>
+							{player.playerName}
+						</Link>
 					</div>
-					<Link
-						href={`/players/${player._id}`}
-						className="font-barlow w-fit truncate text-lg uppercase text-neutral-100 transition hover:opacity-80"
-					>
-						{player.playerName}
-					</Link>
 				</div>
-			</div>
 
-			{/* stats table */}
-			<div className="h-fit border-b border-neutral-600"></div>
+				{/* stats table */}
+				<div className="h-fit border-b border-neutral-600"></div>
 
-			{/* badges */}
-			{/* <div className="flex justify-center gap-x-2 border-b border-neutral-600 px-5 py-3">
+				{/* badges */}
+				{/* <div className="flex justify-center gap-x-2 border-b border-neutral-600 px-5 py-3">
 				{badges.map((badge, index) => {
 					const hasBadge = badge.length > 0;
 
@@ -123,19 +124,20 @@ export default function FeaturedPlayerCard({ player }) {
 				})}
 			</div> */}
 
-			{/* avg stats */}
-			<div className="flex justify-center gap-4 border-b border-neutral-600 px-4 py-3">
-				{avgStats.slice(0, 3).map((stat, index) => (
-					<div
-						key={index}
-						className="font-barlow flex w-full flex-col justify-center rounded-lg bg-neutral-600 py-4"
-					>
-						<h6 className="text-center">{stat.label}</h6>
-						<h3 className="text-center text-[31px] font-medium">
-							{stat.average}
-						</h3>
-					</div>
-				))}
+				{/* avg stats */}
+				<div className="flex justify-center gap-4 border-b border-neutral-600 px-4 py-3">
+					{avgStats.slice(0, 3).map((stat, index) => (
+						<div
+							key={index}
+							className="font-barlow flex w-full flex-col justify-center rounded-lg bg-neutral-600 py-4"
+						>
+							<h6 className="text-center">{stat.label}</h6>
+							<h3 className="text-center text-[31px] font-medium">
+								{stat.average}
+							</h3>
+						</div>
+					))}
+				</div>
 			</div>
 
 			{/* view player profile */}
