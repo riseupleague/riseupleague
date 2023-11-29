a2bea347525ce5013554b468baf3d5fdb2afe828
n9d0g <nathanjlardizabal@gmail.com>
2023-11-29 10:33:36 -0500

    fix(bball): use google photo for avatar; toast messaging wip

--- apps/bball/components/auth/ProfileLink.tsx
+++ apps/bball/components/auth/ProfileLink.tsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "@ui/components/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@ui/components/avatar";
 import { LogOut } from "lucide-react";
 import SignInDialog from "@/components/auth/SignInDialog";
-import ProfileIcon from "../icons/ProfileIcon";
 import PlayerIcon from "../icons/PlayerIcon";
+import DownChevronIcon from "../icons/DownChevronIcon";
+import { useToast } from "@ui/components/use-toast";
+// import { ToastAction } from "@ui/components/toast";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -21,6 +24,7 @@ import {
 const ProfileLink = () => {
 	const { status, data: session } = useSession();
 	const [open, setOpen] = useState(false);
+	const { toast } = useToast()
 
 	const openDialog = () => {
 		setOpen(true);
@@ -28,6 +32,11 @@ const ProfileLink = () => {
 
 	const closeDialog = () => {
 		setOpen(false);
+	};
+
+	const handleLogOut = () => {
+		closeDialog();
+		signOut();
 	};
 
 	return (
@@ -40,44 +49,48 @@ const ProfileLink = () => {
 								<span className="text-primary hidden text-lg lg:inline-block">
 									Welcome back, {session?.user?.name}
 								</span>
-								<span>
-									<ProfileIcon />
-								</span>
+								<div className="flex items-center gap-1">
+									<Avatar>
+										<AvatarImage src={`${session.user.image}`} />
+										<AvatarFallback className="bg-neutral-400 uppercase">
+											{session?.user?.name[0]}
+										</AvatarFallback>
+									</Avatar>
+									<DownChevronIcon />
+								</div>
 							</DropdownMenuTrigger>
-							<DropdownMenuContent className="w-56 bg-neutral-900">
-								<DropdownMenuLabel className="font-barlow text-lg font-medium uppercase">
+							<DropdownMenuContent className="font-barlow w-56 border border-neutral-500 bg-neutral-900">
+								<DropdownMenuLabel className="font-barlow text-xl font-medium uppercase">
 									{session?.user?.name}
 								</DropdownMenuLabel>
-								<DropdownMenuSeparator />
+
+								<DropdownMenuSeparator className="border border-neutral-600" />
+
 								<DropdownMenuGroup>
-									<DropdownMenuItem className="cursor-pointer">
+									<DropdownMenuItem asChild className="cursor-pointer text-lg">
 										<Link
+											onClick={closeDialog}
 											href="/user"
-											className="flex items-center gap-2 transition-all hover:opacity-80"
+											className="flex w-full items-center gap-2 transition-all hover:opacity-80"
 										>
 											<PlayerIcon />
 											<span>Profile</span>
 										</Link>
 									</DropdownMenuItem>
 								</DropdownMenuGroup>
-								<DropdownMenuSeparator />
+
+								<DropdownMenuSeparator className="border border-neutral-600" />
 
 								<DropdownMenuItem
-									className="flex cursor-pointer items-center gap-2 transition-all hover:opacity-80"
-									onClick={() => signOut()}
+									className="flex w-full cursor-pointer items-center gap-2 text-lg transition-all hover:opacity-80"
+									onClick={handleLogOut}
 								>
 									<LogOut className="stroke-neutral-400" />
-									<span>Log out</span>
+									Log out
 								</DropdownMenuItem>
 							</DropdownMenuContent>
 						</DropdownMenu>
-						{/* <span className="font-barlow  font-semibold uppercase ">
-							{session?.user?.name}
-						</span> */}
 					</div>
-					{/* <Button variant="register" >
-						Log Out
-					</Button> */}
 				</>
 			) : (
 				<>
