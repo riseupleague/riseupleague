836ffc6587941689de38dd9b347301aaca703410
johnBaloran <jdbalora@gmail.com>
2023-12-09 17:40:46 -0500

    jersey design in team pages

--- apps/bball/components/teams/team/Overview.tsx
+++ apps/bball/components/teams/team/Overview.tsx
@@ -2,10 +2,67 @@
 
 import ScheduleCard from "@/components/games/ScheduleCard";
 import FeaturedPlayerCard from "@/components/general/FeaturedPlayerCard";
+import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const Overview = ({ team, upcomingGames }) => {
+	console.log(team);
+	const jerseyEdition = team?.jerseyEdition;
+	let edition; // Assuming team.jerseyEdition is a string like "retro-1", "original-1", or "classic-1"
+	if (jerseyEdition) {
+		edition = jerseyEdition.split("-")[0];
+	}
+
+	// Dynamic import of the component
+	const DynamicComponent = dynamic(
+		() => import(`@/lib/jersey-designs/${edition}/${jerseyEdition}`),
+		{
+			loading: () => <p>Loading...</p>, // Component to show while loading
+			ssr: false, // Set to false if you want to disable server-side rendering for this component
+		}
+	);
 	return (
 		<div>
+			<div>
+				<h2 className="font-barlow my-8 text-center text-4xl uppercase">
+					Jersey Design
+				</h2>
+				{jerseyEdition && jerseyEdition !== "" && (
+					<div className=" mx-auto w-full border-neutral-600 bg-neutral-700 lg:w-1/2">
+						<style id="styleElement">
+							{`.primaryColorFill {
+								  			fill: ${team.primaryColor} !important;
+											}
+								.primaryColorStroke {
+									stroke: ${team.primaryColor} !important;
+								  }
+								  .tertiaryColorFill {
+									fill: ${team.tertiaryColor} !important;
+								  }
+								  .tertiaryColorStroke {
+									stroke: ${team.tertiaryColor} !important;
+								  }
+					
+								  .secondaryColorFill {
+									fill: ${team.secondaryColor} !important;
+								  }
+					
+								  .secondaryColorStroke {
+									stroke: ${team.secondaryColor} !important;
+								  }
+
+								  .jerseyDiv {
+									background-color: rgb(17 22 29 / var(--tw-bg-opacity));
+								  }
+							
+								`}
+						</style>
+						<div className="flex flex-col">
+							<DynamicComponent />
+						</div>
+					</div>
+				)}
+			</div>
 			{/* upcoming games grid */}
 			<div>
 				<h2 className="font-barlow my-8 text-center text-4xl uppercase">
