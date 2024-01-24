a49da43b6b0f53ce4669052574071adaf28ebcb6
n9d0g <nathanjlardizabal@gmail.com>
2024-01-24 12:11:12 -0500

    build(bball): build fix -> component imports

--- apps/bball/app/page.tsx
+++ apps/bball/app/page.tsx
@@ -4,8 +4,6 @@ import AboutRiseUp from "@/components/home/AboutRiseUp";
 import PlayersOfTheWeek from "@/components/home/PlayersOfTheWeek";
 import LatestGames from "@/components/home/LatestGames";
 import MVPLadder from "@/components/home/MVPLadder";
-import Socials from "@/components/home/Socials";
-import FAQs from "@/components/home/FAQs";
 import ContactUs from "@/components/home/ContactUs";
 import HomeRegister from "@/components/home/HomeRegister";
 import SecondaryHeader from "@/components/structure/header/secondary-header";
@@ -13,6 +11,8 @@ import { Metadata } from "next";
 import { connectToDatabase } from "@/api-helpers/utils";
 import SetYourScheduleButton from "@/components/home/SetYourScheduleButton";
 import { Suspense } from "react";
+import Socials from "@/components/home/Socials";
+import FAQs from "@/components/home/FAQs";
 
 export const metadata: Metadata = {
 	title: "Rise Up League | Home",
