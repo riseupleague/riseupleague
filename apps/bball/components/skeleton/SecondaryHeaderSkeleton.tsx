b0b0336f80dda3f322df8b73fe0e691af276447d
n9d0g <nathanjlardizabal@gmail.com>
2024-01-25 19:31:43 -0500

    fix(bball): another secondary header skeleton fix

--- apps/bball/components/skeleton/SecondaryHeaderSkeleton.tsx
+++ apps/bball/components/skeleton/SecondaryHeaderSkeleton.tsx
@@ -4,18 +4,16 @@ export default function SecondaryHeaderSkeleton() {
 	const array3 = Array(3).fill("");
 
 	return (
-		<div className="flex items-center overflow-x-auto overflow-y-hidden xl:overflow-x-hidden">
-			<div className="flex h-[140px] items-center rounded-sm border-y border-neutral-600">
-				<div className="flex items-center space-x-4">
-					{array3.map((_, index) => (
-						<div className="space-y-3" key={index}>
-							<Skeleton className="h-4 w-[100px] bg-neutral-700" />
-							<Skeleton className="h-4 w-[120px] bg-neutral-700" />
-							<Skeleton className="h-4 w-[150px] bg-neutral-700" />
-							<Skeleton className="h-4 w-[250px] bg-neutral-700" />
-						</div>
-					))}
-				</div>
+		<div className="flex h-[140px] w-full items-center overflow-x-auto overflow-y-hidden rounded-sm border-y border-neutral-600 xl:overflow-x-hidden">
+			<div className="flex items-center space-x-4">
+				{array3.map((_, index) => (
+					<div className="space-y-3" key={index}>
+						<Skeleton className="h-4 w-[100px] bg-neutral-700" />
+						<Skeleton className="h-4 w-[120px] bg-neutral-700" />
+						<Skeleton className="h-4 w-[150px] bg-neutral-700" />
+						<Skeleton className="h-4 w-[250px] bg-neutral-700" />
+					</div>
+				))}
 			</div>
 		</div>
 	);
