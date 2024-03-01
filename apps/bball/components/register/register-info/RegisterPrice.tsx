ec209d8186dd7a2d85a75dc7ecad5005e715adae
johnBaloran <jdbalora@gmail.com>
2024-03-01 00:11:27 -0500

    player of the week phot update

--- apps/bball/components/register/register-info/RegisterPrice.tsx
+++ apps/bball/components/register/register-info/RegisterPrice.tsx
@@ -1,104 +1,75 @@
 import React from "react";
 import styles from "./RegisterPrice.module.css"; // Import CSS module
+import Image from "next/image";
+import Link from "next/link";
 
 const RegisterPrice = () => {
 	return (
 		<section id={styles["sbsr-1100"]} className={styles["sbsr-1100"]}>
 			<div className={`${styles["cs-container"]} container mx-auto`}>
 				<div className={styles["cs-image-group"]}>
-					<picture
-						className={`${styles["cs-picture"]} ${styles["cs-picture1"]}`}
-					>
-						<source
-							media="(max-width: 600px)"
-							srcSet="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic18.jpg"
-						/>
-						<source
-							media="(min-width: 601px)"
-							srcSet="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic18.jpg"
-						/>
-						<img
+					<div className={`${styles["cs-picture"]} ${styles["cs-picture1"]}`}>
+						<Image
 							loading="lazy"
 							decoding="async"
-							src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic18.jpg"
+							src="/images/register/RegisterPrice1.jpg"
 							alt="appliance"
 							width="323"
 							height="447"
 						/>
-					</picture>
-					<picture
-						className={`${styles["cs-picture"]} ${styles["cs-picture2"]}`}
-					>
-						<source
-							media="(max-width: 600px)"
-							srcSet="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic19.jpg"
-						/>
-						<source
-							media="(min-width: 601px)"
-							srcSet="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic19.jpg"
-						/>
-						<img
+					</div>
+					<div className={`${styles["cs-picture"]} ${styles["cs-picture2"]}`}>
+						<Image
 							loading="lazy"
 							decoding="async"
-							src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/mechanic19.jpg"
+							src="/images/register/RegisterPrice2.jpg"
 							alt="appliance"
-							width="305"
-							height="441"
+							width="323"
+							height="447"
 						/>
-					</picture>
-					<img
+					</div>
+
+					<Image
 						className={styles["cs-stripes"]}
 						loading="lazy"
 						decoding="async"
-						src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/stripes-short.svg"
-						alt="stripes"
+						src="/images/register/RegisterPriceStripes.svg"
+						alt="appliance"
 						width="548"
 						height="487"
-						aria-hidden="true"
 					/>
-					{/* <img
-						className={styles["cs-graphic"]}
-						loading="lazy"
-						decoding="async"
-						src="https://csimg.nyc3.cdn.digitaloceanspaces.com/Icons/eclipses.svg"
-						alt="stripes"
-						width="140"
-						height="140"
-						aria-hidden="true"
-					/> */}
 				</div>
 				<div className={styles["cs-content"]}>
-					<span className={styles["cs-topper"]}>Mission & Vision</span>
-					<h2 className={styles["cs-title"]}>
-						Stitch Car Mechanics Mission & Vision
-					</h2>
+					{/* <span className={styles["cs-topper"]}>Mission & Vision</span> */}
+					<h2 className="text-5xl">Register Now</h2>
 					<p className={styles["cs-text"]}>
-						Our professionals will perform diagnostic tests, fluid flush and
-						fills, engine replacement, oil changes, and total vehicle overhauls.
-						Busto auctor lectus vitae conubia euismod nec rhoncus parturient
-						convallis parturient accumsan volutpat ligula and intege aenean
-						facilisis.
+						Play in the league with the best player experience!
 					</p>
 					<ul className={styles["cs-ul"]}>
 						<li className={styles["cs-li"]}>
-							<h3 className={styles["cs-h3"]}>Mission Statement</h3>
+							<h3 className={styles["cs-h3"]}>
+								Early Bird Price: $210 + Tax Per player OR sign up as a team for
+								only $2000 tax included.
+							</h3>
 							<p className={styles["cs-li-text"]}>
-								Our Mission is to serve our customers and always deliver the
-								highest level of customer service; to develop our team and
-								strive to constantly improve; and to conduct ourselves in an
-								environmentally responsible manner.
+								Early Bird Opens: Feb 23, 2024 at 9:00 PM
+							</p>
+							<p className={styles["cs-li-text"]}>
+								Early Bird Closes: Mar. 8, 2024 at 11:59 PM
 							</p>
 						</li>
 						<li className={styles["cs-li"]}>
-							<h3 className={styles["cs-h3"]}>Our Vision</h3>
 							<p className={styles["cs-li-text"]}>
-								To be the world’s most exciting leader in automotive business
-								intelligence solutions. We will generate excitement through
-								implementing pioneering ideas, problem solving & going beyond
-								our customers’ expectations.
+								Regular Price is $235 + tax per player.{" "}
 							</p>
 						</li>
 					</ul>
+					<Link
+						href="/register"
+						className="font-barlow mt-10 block rounded bg-neutral-100 px-12 py-2 text-center font-semibold uppercase text-neutral-900 transition hover:bg-neutral-200"
+					>
+						Register Now
+					</Link>
 				</div>
 			</div>
 		</section>
