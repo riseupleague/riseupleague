92c4801efa17c9edc774358f20fe9fa22bbd668d
n9d0g <nathanjlardizabal@gmail.com>
2024-03-04 14:40:15 -0500

    fix(vball): social button link fix

--- packages/ui/components/ui/SocialButton.tsx
+++ packages/ui/components/ui/SocialButton.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-const SocialButton = ({ children, href = "/" }): JSX.Element => {
+const SocialButton = ({ children, href = "/" }: any): JSX.Element => {
 	return (
 		<Link
-			href="/"
+			href={href}
 			className="flex size-8 items-center justify-center rounded-full bg-[#484848] px-2 py-2 transition hover:bg-[#6787E7]"
 		>
 			{children}
