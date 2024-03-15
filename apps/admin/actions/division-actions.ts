c54e444256756cc154c8ad35b32d4bd192bdfcb6
Nathan Lardizabal <nate@njil.dev>
2024-03-15 14:19:12 -0400

    refactor(admin): move `addSeason` to `seasons-actions` file

--- apps/admin/actions/division-actions.ts
+++ apps/admin/actions/division-actions.ts
@@ -1,26 +1,7 @@
 "use server";
 
 import Division from "@/api-helpers/models/Division";
-import Season from "@/api-helpers/models/Season";
 import { revalidatePath } from "next/cache";
-
-export async function addSeason(seasonData: any, id: string) {
-	try {
-		console.log(seasonData);
-		return;
-
-		const season = await Season.findById(seasonData._id);
-		if (!season) return { status: 404 };
-
-		await Season.findByIdAndUpdate(seasonData._id, seasonData);
-
-		revalidatePath(`/seasons-management/season/${id}`);
-		return { status: 200 };
-	} catch (e) {
-		console.log(e);
-		return { status: 500 };
-	}
-}
 
 export async function editDivision(divisionData: any, id: string) {
 	try {
--- apps/admin/actions/seasons-actions.ts
+++ apps/admin/actions/seasons-actions.ts
@@ -3,6 +3,24 @@
 import Season from "@/api-helpers/models/Season";
 import { revalidatePath } from "next/cache";
 
+export async function addSeason(seasonData: any, id: string) {
+	try {
+		console.log(seasonData);
+		return;
+
+		const season = await Season.findById(seasonData._id);
+		if (!season) return { status: 404 };
+
+		await Season.findByIdAndUpdate(seasonData._id, seasonData);
+
+		revalidatePath(`/seasons-management/season/${id}`);
+		return { status: 200 };
+	} catch (e) {
+		console.log(e);
+		return { status: 500 };
+	}
+}
+
 export async function editSeason(seasonData: any, id: string) {
 	try {
 		const season = await Season.findById(seasonData._id);
