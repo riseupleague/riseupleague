dbcfb07ef7d8dc85eed762a4c13d161f65d929fe
Nathan Lardizabal <nate@njil.dev>
2024-03-23 19:04:47 -0400

    fix(bball): `user/player` grid order fix

--- apps/bball/components/user/UserPlayerInfo.tsx
+++ apps/bball/components/user/UserPlayerInfo.tsx
@@ -70,7 +70,7 @@ const UserPlayerInfo = ({ player }) => {
 
 	return (
 		<div className="font-barlow grid grid-cols-1 gap-4 uppercase sm:grid-cols-2">
-			<div>
+			<div className="order-last sm:order-first">
 				<UserPlayerSeasonInfo player={playerInfo} />
 
 				<Sheet>
