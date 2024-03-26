e889c883ff7a2b47e9612b1886cce51c6fda07c9
Nathan Lardizabal <nate@njil.dev>
2024-03-26 16:30:10 -0400

    feat(stats): :sparkles: stats directory added; basic scaffold

---
+++ apps/stats/app/layout.tsx
@@ -0,0 +1,33 @@
+import "../../../packages/ui/styles/globals.css";
+import type { Metadata } from "next";
+import { Inter, Oswald, Barlow_Condensed } from "next/font/google";
+import { Toaster } from "@ui/components/toaster";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const oswald = Oswald({ subsets: ["latin"], variable: "--font-oswald" });
+const barlow = Barlow_Condensed({
+	subsets: ["latin"],
+	variable: "--font-barlow",
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const RootLayout = ({ children }): JSX.Element => {
+	return (
+		<html
+			className={`${oswald.variable} ${inter.variable} ${barlow.variable}`}
+			lang="en"
+		>
+			<body className="font-barlow bg-neutral-900 text-neutral-100">
+				<main className="stats">{children}</main>
+				<Toaster />
+			</body>
+		</html>
+	);
+};
+
+export const metadata: Metadata = {
+	title: "Rise Up League | Stat Tracker",
+	description: "Rise Up League Stat Tracker",
+};
+
+export default RootLayout;
