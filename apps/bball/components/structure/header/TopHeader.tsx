12d1657f2866ada6ac4d433b8f257769f74be368
Sofia Andrino <sofiaandrino1998@gmail.com>
2024-05-08 21:19:48 -0400

    links added

--- apps/bball/components/structure/header/TopHeader.tsx
+++ apps/bball/components/structure/header/TopHeader.tsx
@@ -9,9 +9,9 @@ const TopHeader = (): JSX.Element => {
 		<nav>
 		<div className="container mx-auto grid grid-cols-2 py-4 lg:py-4 font-barlow uppercase bg-background: #04080F border-b border-gray-200;">
 			<div className="flex items-center justify-center lg:justify-start">
-					<Link href="/" className="hidden hover:opacity-80 lg:inline-block hover:text-primary text-l transition-all mr-8 focus:text-primary focus:underline active:text-red-500 active:underline">RISEUP.COM</Link>
-					<Link href="/" className="hidden hover:opacity-80 lg:inline-block hover:text-primary text-l transition-all mr-8 focus:text-primary focus:underline active:text-red-500 active:underline">BASKETBALL</Link>
-					<Link href="/" className="hidden hover:opacity-80 lg:inline-block hover:text-primary text-l transition-all focus:text-primary focus:underline active:text-red-500 active:underline">VOLLEYBALL</Link>
+					<Link href="https://riseupleague.com" className="hidden hover:opacity-80 lg:inline-block hover:text-primary text-l transition-all mr-8 focus:text-primary focus:underline active:text-red-500 active:underline">RISEUP.COM</Link>
+					<Link href="https://bball.riseupleague.com" className="hidden hover:opacity-80 lg:inline-block hover:text-primary text-l transition-all mr-8 focus:text-primary focus:underline active:text-red-500 active:underline">BASKETBALL</Link>
+					<Link href="https://vball.riseupleague.com" className="hidden hover:opacity-80 lg:inline-block hover:text-primary text-l transition-all focus:text-primary focus:underline active:text-red-500 active:underline">VOLLEYBALL</Link>
 			</div>
 			<div className="flex items-center justify-end">
 				<Link href="https://www.facebook.com/riseup.bball" className="flex size-8 items-center justify-center px-2 py-2">
