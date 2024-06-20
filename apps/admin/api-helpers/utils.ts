28e73145ec1d5fdab3d8aa64c3a1004daeef1d2b
Nathan Lardizabal <nate@njil.dev>
2024-06-20 13:22:00 -0400

    fix(admin): :bug: utils file fix

--- apps/admin/api-helpers/utils.ts
+++ apps/admin/api-helpers/utils.ts
@@ -1,34 +1,7 @@
-// import mongoose from "mongoose";
-
-// // eslint-disable-next-line turbo/no-undeclared-env-vars
-// const uri: string = process.env.MONGODB_URI as string;
-
-// export const connectToDatabase = async () => {
-// 	try {
-// 		await mongoose.connect(uri);
-
-// 		require("./models/Division");
-// 		require("./models/Game");
-// 		require("./models/Player");
-// 		require("./models/Season");
-// 		require("./models/Team");
-// 		require("./models/User");
-
-// 		console.log("connected to mongoDB");
-// 	} catch (e) {
-// 		console.log("cannot connect to mongoDB: ", e);
-// 	}
-// };
 import mongoose from "mongoose";
 
 // eslint-disable-next-line turbo/no-undeclared-env-vars
 const uri: string = process.env.MONGODB_URI as string;
-
-let cached = globalThis.mongoose;
-
-if (!cached) {
-	cached = globalThis.mongoose = { conn: null, promise: null };
-}
 
 let cached = globalThis.mongoose;
 
@@ -52,7 +25,6 @@ export const connectToDatabase = async () => {
 			require("./models/Player");
 			require("./models/Season");
 			require("./models/Team");
-			// require("./models/Worker");
 			require("./models/User");
 			return mongoose;
 		});
@@ -62,14 +34,7 @@ export const connectToDatabase = async () => {
 		cached.conn = await cached.promise;
 		console.log("Connected to MongoDB");
 		return cached.conn;
-	try {
-		cached.conn = await cached.promise;
-		console.log("Connected to MongoDB");
-		return cached.conn;
 	} catch (e) {
-		cached.promise = null;
-		console.error("Cannot connect to MongoDB: ", e);
-		throw e;
 		cached.promise = null;
 		console.error("Cannot connect to MongoDB: ", e);
 		throw e;
