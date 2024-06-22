57231bae329cb7cd72bbd36ac5e8e90221897574
johnBaloran <jdbalora@gmail.com>
2024-06-22 15:19:07 -0400

    login fix

--- apps/admin/app/api/auth/[...nextauth]/route.ts
+++ apps/admin/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,49 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Worker from "@/api-helpers/models/Worker";
+import { LoginSchema } from "@/schemas";
 
-import { authOptions } from "./options"; // Adjust the path as necessary
+const authOptions: NextAuthOptions = {
+	session: {
+		strategy: "jwt",
+		maxAge: 30 * 24 * 60 * 60, // 30 days
+	},
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				const worker = await Worker.findOne({ email: user.email }).select(
+					"type"
+				);
+				token.type = worker ? worker.type : null;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			session.user.type = token.type as string;
+			return session;
+		},
+	},
+	providers: [
+		CredentialsProvider({
+			name: "credentials",
+			credentials: {
+				email: { label: "Email", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials) {
+				const validatedFields = LoginSchema.safeParse(credentials);
+				if (validatedFields.success) {
+					const { email, password } = validatedFields.data;
+					const worker = await Worker.findOne({ email });
+					if (worker && worker.password === password) {
+						return worker;
+					}
+				}
+				return null;
+			},
+		}),
+	],
+};
 
 const handler = NextAuth(authOptions as any);
 export { handler as GET, handler as POST };
