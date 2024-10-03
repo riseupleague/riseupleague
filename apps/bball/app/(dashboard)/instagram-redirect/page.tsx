67b785151822564c579f66acb72ac50b24d8d6bb
Nathan Lardizabal <nate@njil.dev>
2024-10-02 20:56:59 -0400

    build(bball): :bug: build fix -> navigator in `useEffect`

--- apps/bball/app/(dashboard)/instagram-redirect/page.tsx
+++ apps/bball/app/(dashboard)/instagram-redirect/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link";
 import { Button } from "@ui/components/button";
 
--- apps/bball/components/home/HomeRegister.tsx
+++ apps/bball/components/home/HomeRegister.tsx
@@ -5,12 +5,15 @@ import Link from "next/link";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 const HomeRegister = (): JSX.Element => {
 	const { data: session } = useSession();
 
-	if (navigator.userAgent.includes("Instagram"))
-		redirect("/instagram-redirect");
+	useEffect(() => {
+		if (navigator.userAgent.includes("Instagram"))
+			redirect("/instagram-redirect");
+	}, []);
 
 	return (
 		<section className="relative mb-16 flex h-[500px] max-h-[100dvh-300px] flex-col items-center justify-center text-center lg:mb-24 lg:h-[750px] xl:h-[1000px]">
